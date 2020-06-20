eb70b2ef944aac6473a212ae7878cdf0f5c351bf
Zaharia Justinian Darius <justinian.zaharia@gmail.com>
2020-06-20 23:29:30 +0300

    FIS1-81_Sortare_si_filtrare_produse

--- src/main/java/ListaProdusePetshop.java
+++ src/main/java/ListaProdusePetshop.java
@@ -52,4 +52,101 @@ public class ListaProdusePetshop {
             e.printStackTrace();
         }
     }
+
+    public void filt_and_sort() {
+        String[] c ={"HRANA","INGRIJIRE","JUCARII"};
+        boolean[] f={false,false,false},s ={false,false,false,false};
+        try {
+            if (hrana.isSelected()) f[0] = true;
+            if (ingrijire.isSelected()) f[1] = true;
+            if (jucarii.isSelected()) f[2] = true;
+            if(az.isSelected()&&az_state==0)
+            {
+                if(price.isSelected())
+                {
+                    price.setSelected(false);
+                    price_state =0;
+                    price.setText("Pret (crescator)");
+                }
+                s[0]=true;
+                az_state=1;
+            }
+            else if(az.isSelected()&&az_state==1)
+                s[0]=true;
+            else if(!az.isSelected()&&az_state==1)
+            {
+                az.setText("Alfabetic (Z-A)");
+                az.setSelected(true);
+                s[1]=true;
+                az_state=2;
+            }
+            else if(az.isSelected()&&az_state==2)
+                s[1]=true;
+            else if(!az.isSelected()&&az_state==2)
+            {
+                az.setText("Alfabetic (A-Z)");
+                az_state=0;
+            }
+
+            if(price.isSelected()&& price_state ==0)
+            {
+                if(az.isSelected())
+                {
+                    s[0]=false;s[1]=false;
+                    az.setSelected(false);
+                    az_state=0;
+                    az.setText("Alfabetic (A-Z)");
+                }
+                s[2]=true;
+                price_state =1;
+            }
+            else if(price.isSelected()&&price_state==1)
+                s[2]=true;
+            else if(!price.isSelected()&& price_state ==1)
+            {
+                price.setText("Pret (descrescator)");
+                price.setSelected(true);
+                s[3]=true;
+                price_state =2;
+            }
+            else if(price.isSelected()&&price_state==2)
+                s[3]=true;
+            else if(!price.isSelected()&& price_state ==2)
+            {
+                price.setText("Pret (crescator)");
+                price_state =0;
+            }
+        } catch (Exception e) { }
+        ArrayList<Product> filt = new ArrayList<Product>();
+        ArrayList<Product> filt_and_sort = new ArrayList<Product>();
+        lv.getItems().clear();
+        if(f[0]||f[1]||f[2]) {
+            for (AllProducts p : all_products)
+                if (p.getObject().equals("PRODUCT") && ((Product) p).getMagazin().equals(shop_username))
+                    if (((Product) p).getCategorie().equalsIgnoreCase(c[0]) && f[0]
+                            || ((Product) p).getCategorie().equalsIgnoreCase(c[1]) && f[1]
+                            || ((Product) p).getCategorie().equalsIgnoreCase(c[2]) && f[2])
+                        filt.add(((Product)p));
+        }
+        else
+            for (AllProducts p : all_products)
+                if (p.getObject().equals("PRODUCT") && ((Product) p).getMagazin().equals(shop_username))
+                    filt.add(((Product)p));
+        if(!filt.isEmpty()) {
+            if (s[0]||s[1]||s[2]||s[3]) {
+                int i,n=filt.size();
+                for (i = 0; i < n; i++) {
+                    Product aux = filt.get(0);
+                    for (Product p : filt)
+                        if(s[0] && p.getNume().toUpperCase().compareTo(aux.getNume().toUpperCase())<0 || s[1] && p.getNume().toUpperCase().compareTo(aux.getNume().toUpperCase())>0 || s[2] && p.getPret() < aux.getPret() || s[3] && p.getPret() > aux.getPret())
+                            aux = p;
+                    filt_and_sort.add(aux);
+                    filt.remove(aux);
+                }
+            }
+            else filt_and_sort.addAll(filt);
+            for (Product p : filt_and_sort)
+                lv.getItems().add(p.toLV());
+        }
+    }
 }
