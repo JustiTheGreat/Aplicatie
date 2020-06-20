bee2c5ccf2dbe73d3dc15869f96d9247ae67d02c
Zaharia Justinian Darius <justinian.zaharia@gmail.com>
2020-06-21 01:03:31 +0300

    FIS1-59_Se_intorduc_si_se_salveaza_datele_noului_animal

--- src/main/java/AdaugaAnimal.java
+++ src/main/java/AdaugaAnimal.java
@@ -36,5 +36,210 @@ public class AdaugaAnimal {
     private ChoiceBox sex;
 
     public void set(Stage stage, String shop_username,ArrayList<AllProducts> all_products, ListView<String> lv, RadioButton fem, RadioButton masc, RadioButton pisici, RadioButton caini, RadioButton papagali, RadioButton pestisori, RadioButton hamsteri, RadioButton az, RadioButton dn,int az_state,int dn_state) {
+        this.stage = stage;
+        this.shop_username = shop_username;
+        this.all_products = all_products;
+        this.lv = lv;
+        this.fem=fem;
+        this.masc=masc;
+        this.pisici=pisici;
+        this.caini=caini;
+        this.papagali=papagali;
+        this.pestisori=pestisori;
+        this.hamsteri=hamsteri;
+        this.az=az;
+        this.dn=dn;
+        sex.getItems().addAll("FEMININ","MASCULIN");
+        tip.getItems().addAll("PISICI","CAINI","PAPAGALI","PESTISORI","HAMSTERI");
+        if (az.isSelected())
+        {
+            this.dn.setSelected(false);
+            this.dn_state=0;
+            if (az_state == 1) {
+                this.az.setSelected(true);
+                this.az_state = 0;
+            } else if (az_state == 2) {
+                this.az.setSelected(false);
+                this.az_state = 1;
+            }
+        }
+        else if (dn.isSelected())
+        {
+            this.az.setSelected(false);
+            this.az_state=0;
+            if (dn_state == 1) {
+                this.dn.setSelected(true);
+                this.dn_state = 0;
+            } else if (dn_state == 2) {
+                this.dn.setSelected(false);
+                this.dn_state = 1;
+            }
+        }
+        else
+        {
+            this.az.setSelected(false);
+            this.az_state=0;
+            this.dn.setSelected(false);
+            this.dn_state=0;
+        }
+    }
+
+    public boolean isInt(String s) {
+        try{
+            Integer.parseInt(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void close()
+    {
+        stage.close();
+    }
+
+    public void addLV () {
+        try {
+            int ok = 1;
+            for (AllProducts p : all_products)
+                if (p.getObject().equals("ANIMAL") && ((Animal) p).getCentru().equals(shop_username) && ((Animal) p).getNumeRasa().equals(nume.getText()))
+                    ok = 0;
+            if (ok == 1 && isInt(data.getText()) && Integer.parseInt(data.getText()) >= 0 && !nume.getText().isEmpty() && !data.getText().isEmpty() && sex != null && tip!=null) {
+                all_products.add(new Animal(nume.getText(),tip.getValue().toString(), sex.getValue().toString(), Integer.parseInt(data.getText()), shop_username, "DISPONIBIL"));
+                write();
+                filt_and_sort();
+                stage.close();
+            } else {
+                nume.setText("");
+                data.setText("");
+                tip.setValue("");
+                sex.setValue("");
+                message("Eroare!", "Date introduse gresit!");
+            }
+        }catch(Exception e){message("Eroare!", "Categorie neselectata!");}
+    }
+    public void write() {
+        try {
+            FileWriter myWriter = new FileWriter("src/main/produse.txt", false);
+            for (AllProducts p : all_products)
+                myWriter.write(p.getObject() + " " + p.toString() + "\n");
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void message(String title,String message) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Message.fxml"));
+            Parent fxml = loader.load();
+            Stage stage = new Stage();
+            Message controller = loader.getController();
+            controller.set(message, stage);
+            stage.setTitle(title);
+            stage.setScene(new Scene(fxml));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void filt_and_sort() {
+        String[] c ={"FEM","MASC","PISICI","CAINI","PAPAGALI","PESTISORI","HAMSTERI"};
+        boolean[] f={false,false,false,false,false,false,false},s ={false,false,false,false};
+        try {
+            if (fem.isSelected()) f[0] = true;
+            if (masc.isSelected()) f[1] = true;
+            if (pisici.isSelected()) f[2] = true;
+            if (caini.isSelected()) f[3] = true;
+            if (papagali.isSelected()) f[4] = true;
+            if (pestisori.isSelected()) f[5] = true;
+            if (hamsteri.isSelected()) f[6] = true;
+            if(az.isSelected()&&az_state==0)
+            {
+                if(dn.isSelected())
+                {
+                    dn.setSelected(false);
+                    dn_state =0;
+                    dn.setText("Data nasterii (crescator)");
+                }
+                s[0]=true;
+                az_state=1;
+            }
+            else if(az.isSelected()&&az_state==1)
+                s[0]=true;
+            else if(!az.isSelected()&&az_state==1)
+            {
+                az.setText("Alfabetic (Z-A)");
+                az.setSelected(true);
+                s[1]=true;
+                az_state=2;
+            }
+            else if(az.isSelected()&&az_state==2)
+                s[1]=true;
+            else if(!az.isSelected()&&az_state==2)
+            {
+                az.setText("Alfabetic (A-Z)");
+                az_state=0;
+            }
+
+            if(dn.isSelected()&& dn_state ==0)
+            {
+                if(az.isSelected())
+                {
+                    s[0]=false;s[1]=false;
+                    az.setSelected(false);
+                    az_state=0;
+                    az.setText("Alfabetic (A-Z)");
+                }
+                s[2]=true;
+                dn_state =1;
+            }
+            else if(dn.isSelected()&&dn_state==1)
+                s[2]=true;
+            else if(!dn.isSelected()&&dn_state ==1)
+            {
+                dn.setText("Pret (descrescator)");
+                dn.setSelected(true);
+                s[3]=true;
+                dn_state =2;
+            }
+            else if(dn.isSelected()&&dn_state==2)
+                s[3]=true;
+            else if(!dn.isSelected()&&dn_state ==2)
+            {
+                dn.setText("Pret (crescator)");
+                dn_state =0;
+            }
+        } catch (Exception e) { }
+        ArrayList<Animal> filt = new ArrayList<Animal>();
+        ArrayList<Animal> filt_and_sort = new ArrayList<Animal>();
+        lv.getItems().clear();
+        if(f[0]||f[1]||f[2]) {
+            for (AllProducts p : all_products)
+                if (p.getObject().equals("ANIMAL") && ((Animal) p).getCentru().equals(shop_username))
+                    if (((Animal) p).getTip().equalsIgnoreCase(c[0]) && f[0] || ((Animal) p).getTip().equalsIgnoreCase(c[1]) && f[1] || ((Animal) p).getTip().equalsIgnoreCase(c[2]) && f[2] || ((Animal) p).getTip().equalsIgnoreCase(c[3]) && f[3] || ((Animal) p).getTip().equalsIgnoreCase(c[4]) && f[4] || ((Animal) p).getTip().equalsIgnoreCase(c[5]) && f[5] || ((Animal) p).getTip().equalsIgnoreCase(c[6]) && f[6])
+                        filt.add(((Animal)p));
+        }
+        else
+            for (AllProducts p : all_products)
+                if (p.getObject().equals("ANIMAL") && ((Animal) p).getCentru().equals(shop_username))
+                    filt.add(((Animal)p));
+        if(!filt.isEmpty()) {
+            if (s[0]||s[1]||s[2]||s[3]) {
+                int i,n=filt.size();
+                for (i = 0; i < n; i++) {
+                    Animal aux = filt.get(0);
+                    for (Animal p : filt)
+                        if(s[0] && p.getNumeRasa().toUpperCase().compareTo(aux.getNumeRasa().toUpperCase())<0 || s[1] && p.getNumeRasa().toUpperCase().compareTo(aux.getNumeRasa().toUpperCase())>0 || s[2] && p.getDataNasterii() < aux.getDataNasterii() || s[3] && p.getDataNasterii() > aux.getDataNasterii())
+                            aux = p;
+                    filt_and_sort.add(aux);
+                    filt.remove(aux);
+                }
+            }
+            else filt_and_sort.addAll(filt);
+            for (Animal p : filt_and_sort)
+                lv.getItems().add(p.toLV());
+        }
     }
 }
